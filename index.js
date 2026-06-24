082e6225c8cade7c4de3e99c7c0fefe9eb65abaa
nehakrr72 <nehakrr72@gmail.com>
2026-06-23 21:25:08 -0400

    Refactor index.js to export app module
    Removed dotenv configuration and server listening code.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const userRoutes = require('./routes/users');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
@@ -12,6 +10,4 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+module.exports = app;
