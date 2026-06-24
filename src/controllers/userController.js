7a305528437efdb6f9db187f1ee47d62b1f04f6c
nehakrr72 <nehakrr72@gmail.com>
2026-06-23 21:52:03 -0400

    Implement user controller with CRUD operations

---
+++ src/controllers/userController.js
@@ -0,0 +1,20 @@
+let users = [
+  { id: 1, name: 'Neha' },
+  { id: 2, name: 'Sam' }
+];
+
+exports.getAllUsers = (req, res) => {
+  res.json(users);
+};
+
+exports.getUserById = (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).json({ message: 'User not found' });
+  res.json(user);
+};
+
+exports.createUser = (req, res) => {
+  const newUser = { id: users.length + 1, name: req.body.name };
+  users.push(newUser);
+  res.status(201).json(newUser);
+};
